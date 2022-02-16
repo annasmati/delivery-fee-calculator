799e2d4d490b714c7928f622d85d9a2fe18e2239
Anna Smati <anna.smati@gmail.com>
2022-02-16 23:08:02 +0200

    Improved SCSS selectors

--- src/components/PricingGuide.tsx
+++ src/components/PricingGuide.tsx
@@ -48,18 +48,20 @@ const PricingGuide: React.FC = () => {
         </EuiFlyoutHeader>
         <EuiFlyoutBody>
           <EuiText>
-            <p>The following elements will have an affect on the total price of delivery:</p>
-            <ul>
-              <li>
-                <p>
+            <p className="pricing-paragraph">
+              The following elements will have an affect on the total price of delivery:
+            </p>
+            <ul className="pricing-ul">
+              <li className="pricing-li">
+                <p className="pricing-paragraph">
                   <strong>Cart Value</strong>
                 </p>
                 If the cart value is less than {minCartValueAsString}€, a small order surcharge is
                 added. The surcharge is calculated by subtracting the cart value from{' '}
                 {minCartValueAsString}€.
               </li>
-              <li>
-                <p>
+              <li className="pricing-li">
+                <p className="pricing-paragraph">
                   <strong>Delivery Distance</strong>
                 </p>
                 The delivery fee for the first {minDeliveryDistanceAsString} meters is{' '}
@@ -67,29 +69,29 @@ const PricingGuide: React.FC = () => {
                 {additionalDistanceDivisorAsString} meters, {additionalDistanceSurchargeAsString}€
                 is added.
               </li>
-              <li>
-                <p>
+              <li className="pricing-li">
+                <p className="pricing-paragraph">
                   <strong>Amount of items</strong>
                 </p>
                 If the number of items exceeds {minItemAmountAsString}, an additional{' '}
                 {additionalItemSurchargeAsString}€ surcharge is added for each item.
               </li>
-              <li>
-                <p>
+              <li className="pricing-li">
+                <p className="pricing-paragraph">
                   <strong>Rush Hour</strong>
                 </p>
                 During the {rushHourDayAsString} rush ({rushHourStartAsString} -{' '}
                 {rushHourEndAsString} UTC), a {rushHourMultiplierAsString}% increase is added to the
                 total fee (total fee including possible surcharges).
               </li>
-              <li>
-                <p>
+              <li className="pricing-li">
+                <p className="pricing-paragraph">
                   <strong>Maximum Delivery Price</strong>
                 </p>
                 The total price of delivery will never exceed {maxDeliveryFeeAsString}€.
               </li>
-              <li>
-                <p>
+              <li className="pricing-li">
+                <p className="pricing-paragraph">
                   <strong>Free Delivery</strong>
                 </p>
                 The delivery is free (0€) when the cart value is equal to or more than{' '}
